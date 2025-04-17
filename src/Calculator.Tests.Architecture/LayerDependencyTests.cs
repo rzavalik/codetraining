d39893ed5e17f3ee3dbdbc5d9b1253f4f77331a2
Rodrigo Castro <zavalik@live.com>
2025-04-17 16:52:10 -0300

    Adding Architectural Tests to prevent incorrect usage of Layers

---
+++ src/Calculator.Tests.Architecture/LayerDependencyTests.cs
@@ -0,0 +1,33 @@
+﻿namespace Calculator.Tests.Architecture;
+
+using System.Reflection;
+using NetArchTest.Rules;
+using Shouldly;
+
+public class LayerDependencyTests
+{
+    [Theory]
+    [InlineData("Calculator.Application")]
+    [InlineData("Calculator.Domain")]
+    [InlineData("Calculator.Infrastructure")]
+    [InlineData("Calculator.API")]
+    [InlineData("Calculator.UI")]
+    public void Assembly_Should_Not_Have_Dependency_On_Test_Assemblies(string assemblyName)
+    {
+        Assembly.Load(assemblyName);
+
+        var assembly = AppDomain.CurrentDomain
+            .GetAssemblies()
+            .FirstOrDefault(a => a.GetName().Name == assemblyName);
+
+        assembly.ShouldNotBeNull($"Assembly {assemblyName} should be loaded for test.");
+
+        var result = Types
+            .InAssembly(assembly)
+            .ShouldNot()
+            .HaveDependencyOnAny("*.Tests.*")
+            .GetResult();
+
+        result.IsSuccessful.ShouldBeTrue($"{assemblyName} should not depend on any test project.");
+    }
+}
